780c266caffb95ff7821134a21c355a8939ee09a
Nhism <111417505+nhismbbi0410@users.noreply.github.com>
2023-04-22 10:34:58 +0700

    Add files via upload

---
+++ src/models/index.js
@@ -0,0 +1,3 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Giangvien = require('./giangvien.model');
---
+++ src/validations/index.js
@@ -0,0 +1,3 @@
+module.exports.authValidation = require('./auth.validation');
+module.exports.userValidation = require('./user.validation');
+module.exports.giangvienValidation = require('./giangvien.validation');
